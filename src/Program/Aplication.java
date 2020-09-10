b14184fb5248e13c7974f6b92344e16ff8a265d7
LuisEduardo21 <lfdasilva9@gmail.com>
2020-09-10 20:52:12 -0300

    adicionando as primeiras classes.

---
+++ src/Program/Aplication.java
@@ -0,0 +1,12 @@
+package Program;
+
+import model.enities.Departament;
+
+public class Aplication {
+
+    public static void main(String[] args){
+
+        Departament obj = new Departament(1, "books");
+        System.out.println(obj);
+    }
+}
---
+++ src/model/enities/Departament.java
@@ -0,0 +1,56 @@
+package model.enities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Departament implements Serializable {
+
+    private Integer id;
+    private String name;
+
+    public Departament(){
+    }
+
+    public Departament(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Departament)) return false;
+        Departament that = (Departament) o;
+        return getId().equals(that.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+
+    @Override
+    public String toString() {
+        return "Departament{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+}
