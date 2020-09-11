3f20f47afad5a0f877d9632f261753ea5b39e977
LuisEduardo21 <lfdasilva9@gmail.com>
2020-09-10 21:06:23 -0300

    adicionando a classe de vendedor.

--- src/Program/Aplication.java
+++ src/Program/Aplication.java
@@ -1,12 +1,16 @@
 package Program;
 
 import model.enities.Departament;
+import model.enities.Seller;
+
+import java.util.Date;
 
 public class Aplication {
 
     public static void main(String[] args){
 
         Departament obj = new Departament(1, "books");
-        System.out.println(obj);
+        Seller seller = new Seller(21, "bob", "bob@gmail.com", new Date(), 30000.0, obj);
+        System.out.println(seller);
     }
 }
