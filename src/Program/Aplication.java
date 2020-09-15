470ab5461882d5f58b5938763116d66c211eb9df
LuisEduardo21 <lfdasilva9@gmail.com>
2020-09-15 19:48:04 -0300

    implementando o codigo do findByInsert.

--- src/Program/Aplication.java
+++ src/Program/Aplication.java
@@ -25,10 +25,14 @@ public class Aplication {
         for (Seller obj : list){
             System.out.println(obj);
         }
-        System.out.println("\n=== TEST 2: seller FindByAll ===");
+        System.out.println("\n=== TEST 3: seller FindByAll ===");
         list = sellerDao.findAll();
         for (Seller obj : list){
             System.out.println(obj);
         }
+        System.out.println("\n=== TEST 4: seller FindByAll ===");
+        Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, departament);
+        sellerDao.insert(newSeller);
+        System.out.println("Inserted! new id = " + newSeller.getId());
     }
 }
